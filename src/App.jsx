d60b5a78c3730523c907e4c41317de359efbdd9e
Wealthometer <wealthhajoh87@gmail.com>
2025-07-19 16:24:56 +0100

    Update.... & ....Upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { Toaster } from 'react-hot-toast'
 import './index.css'
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+import CategoryShop from './pages/CategoryShop'
+import ProductsDetails from './pages/ProductsDetails'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <main>
+      <Header />
       <Toaster position='bottom-right' />
       <Routes>
-        
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/shop/category' element={<CategoryShop />} />
+        <Route path='/shop/category/:id' element={<ProductsDetails />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
+      <Footer />
     </main>
   )
 }
