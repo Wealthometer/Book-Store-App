7725d13d850919b888a57cc7eb2c67ecb99d0df1
Wealthometer <wealthhajoh87@gmail.com>
2025-07-19 19:27:06 +0100

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import logoImg from '../assets/logo.png'
+import userImg from '../assets/user.png'
+import { FaBars, FaBarsStaggered } from "react-icons/fa6"
+import { FaSearch } from "react-icons/fa"
+import { RiUserLine } from "react-icons/ri"
+import Navbar from './Navbar'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
 
-export default Header
+    const [menuOpened, setmenuOpened] = useState(false)
+    const [showSearch, setshowSearch] = useState(false)
+
+    return (
+    <header>
+      {/* Logo */}
+      <div className="flex flex-1">
+        <Link to={"/"} className="bold-22 xl:bold-28 flex items-end gap-1">
+            <img src={logoImg} alt="logo" className="hidden sm:block h-12" />
+            <div  className="sm:relative top-1 5">
+                Zibook<span className="text-secondary">a.</span>
+            </div>
+        </Link>
+      </div>
+
+      {/* Navbar for mobile device */}
+      <div className="flex-1">
+        <Navbar setmenuOpened={setmenuOpened} containerStyles={`${menuOpened ? "flex" : "hidden"}`} />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { IoLibraryOutline } from 'react-icons/io5'
+import { PiEnvelopeDuotone } from 'react-icons/pi'
+import { TbBrandBlogger, TbHome } from 'react-icons/tb'
+import { NavLink } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({containerStyles, setmenuOpened}) => {
+
+  const navItems = [
+    {to : "/", label: "Home", icon : <TbHome />},
+    {to : "/shop", label: "Shop", icon : <IoLibraryOutline />},
+    {to : "/blog", label: "Blog", icon : <TbBrandBlogger />},
+    {to : "malito:infozibooka.com", label: "Contact", icon : <PiEnvelopeDuotone />},
+  ]
   return (
-    <div>Navbar</div>
+    <nav className={{containerStyles, setmenuOpened}}>
+      {navItem.map(({to, label, icon}) => (
+        <div key={label}>
+          <NavLink to={to} className={"flex"}>
+            <span className='text-xl'>{icon}</span>
+            <span className='medium-16'>{label}</span>
+          </NavLink>
+        </div>
+      ))}
+    </nav>
   )
 }
 
