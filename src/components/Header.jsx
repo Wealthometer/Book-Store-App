7842390fa99e72437d92ab22e56ff006c81e333d
Wealthometer <wealthhajoh87@gmail.com>
2025-07-21 22:23:56 +0100

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,40 @@
 import React from "react";
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import logoImg from '../assets/logo.png'
-import userImg from '../assets/user.png'
-import { FaBars, FaBarsStaggered } from "react-icons/fa6"
-import { FaSearch } from "react-icons/fa"
-import { RiUserLine } from "react-icons/ri"
-import Navbar from './Navbar'
+import logoImg from "../assets/logo.png";
+import userImg from "../assets/user.png";
+import { FaBars, FaBarsStaggered } from "react-icons/fa6";
+import { FaSearch } from "react-icons/fa";
+import { RiUserLine } from "react-icons/ri";
+import Navbar from "./Navbar";
+import '../index.css'
 
 const Header = () => {
+  const [menuOpened, setmenuOpened] = useState(true);
+  const [showSearch, setshowSearch] = useState(false);
 
-    const [menuOpened, setmenuOpened] = useState(false)
-    const [showSearch, setshowSearch] = useState(false)
-
-    return (
+  return (
     <header>
       {/* Logo */}
       <div className="flex flex-1">
         <Link to={"/"} className="bold-22 xl:bold-28 flex items-end gap-1">
-            <img src={logoImg} alt="logo" className="hidden sm:block h-12" />
-            <div  className="sm:relative top-1 5">
-                Zibook<span className="text-secondary">a.</span>
-            </div>
+          <img src={logoImg} alt="logo" className="hidden sm:block h-12" />
+          <div className="sm:relative top-1 5">
+            Zibook<span className="text-secondary">a.</span>
+          </div>
         </Link>
       </div>
 
       {/* Navbar for mobile device */}
       <div className="flex-1">
-        <Navbar setmenuOpened={setmenuOpened} containerStyles={`${menuOpened ? "flex" : "hidden"}`} />
+        <Navbar
+          setmenuOpened={setmenuOpened}
+          containerStyles={`${
+            menuOpened
+              ? "flex items-start flex-col gap-y-8 fixed top-16 right-6 p-5 bg-white rounded-full shadow-md w-52 ring-1 ring-slate-900/5 z-50"
+              : "hidden lg:flex gap-x-5 xl:gap-x-7 medium-15 ring-1 ring-1 ring-slate-900/15 rounded-full p-1"
+          }`}
+        />
       </div>
     </header>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { IoLibraryOutline } from 'react-icons/io5'
 import { PiEnvelopeDuotone } from 'react-icons/pi'
 import { TbBrandBlogger, TbHome } from 'react-icons/tb'
 import { NavLink } from 'react-router-dom'
+import '../index.css'
 
 const Navbar = ({containerStyles, setmenuOpened}) => {
 
@@ -13,10 +14,10 @@ const Navbar = ({containerStyles, setmenuOpened}) => {
     {to : "malito:infozibooka.com", label: "Contact", icon : <PiEnvelopeDuotone />},
   ]
   return (
-    <nav className={{containerStyles, setmenuOpened}}>
-      {navItem.map(({to, label, icon}) => (
+    <nav className={containerStyles}>
+      {navItems.map(({to, label, icon}) => (
         <div key={label}>
-          <NavLink to={to} className={"flex"}>
+          <NavLink to={to} className={({isActive}) => `${isActive ? "bg-white ring-1 ring-slate-900" : ""} flexCenter gap-x-2 px-3 py-1.5 rounded-b-full`}>
             <span className='text-xl'>{icon}</span>
             <span className='medium-16'>{label}</span>
           </NavLink>
